0964fededecf17afa2792cb52adab1c3bf4e6434
Gema Disperakim <gema.disperakim@gmail.com>
2022-09-25 06:05:39 +0700

    tes

--- todo-ninja/src/views/HomeView.vue
+++ todo-ninja/src/views/HomeView.vue
@@ -1,23 +1,6 @@
 <template>
   <div class="home">
     <h1>This is an home page</h1>
-    <p class="red white--text">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam similique
-      numquam iste, quasi expedita nam enim, ipsum rerum et deserunt
-      voluptatibus veniam eos maiores. Obcaecati praesentium unde nam ex
-      aliquam?
-    </p>
-    <p class="pink lighten-4 red--text text--darken-4">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam quam
-      odio corporis recusandae reprehenderit temporibus aliquid ipsa non! Dicta,
-      molestias consequuntur laboriosam odit earum accusamus ipsa. Nostrum non
-      quasi accusantium.
-    </p>
-    <h1 class="display-4">Massive display</h1>
-    <h4 class="display-1">Smaller Display</h4>
-    <p class="heading">this is a headlin</p>
-    <p class="subheading font-weight-bold">this is sub heading</p>
-    <p class="caption">this is a caption</p>
   </div>
 </template>
 
