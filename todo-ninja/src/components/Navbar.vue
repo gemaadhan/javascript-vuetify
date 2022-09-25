a211a84732dc7ed3d8a9a5a7b605732cc0c2394e
Gema Disperakim <gema.disperakim@gmail.com>
2022-09-25 13:39:21 +0700

    update

--- todo-ninja/src/components/Navbar.vue
+++ todo-ninja/src/components/Navbar.vue
@@ -13,8 +13,24 @@
     </v-app-bar>
 
     <v-navigation-drawer app v-model="drawer" class="primary">
-      tes</v-navigation-drawer
-    >
+      <v-list>
+        <v-list-item
+          v-for="link in links"
+          :key="link.text"
+          router
+          :to="link.route"
+        >
+          <v-list-item-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">
+              {{ link.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </nav>
 </template>
 
@@ -24,6 +40,11 @@ export default {
   data() {
     return {
       drawer: true,
+      links: [
+        { icon: "mdi-view-dashboard", text: "Dashboard", route: "/" },
+        { icon: "mdi-folder", text: "My Projects", route: "/projects" },
+        { icon: "mdi-account", text: "Team", route: "/team" },
+      ],
     };
   },
 };
